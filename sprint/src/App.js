addd7842530426c647a0832adb940cdb72f124f5
m3l1k4 <melikaihelas@gmail.com>
2020-06-20 18:36:45 -0700

    renders next vid list from json

--- sprint/src/App.js
+++ sprint/src/App.js
@@ -1,22 +1,32 @@
 import React from 'react';
+import axios from 'axios'
 //import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar'
 import CurrentVid from './components/CurrentVid'
-import { render } from '@testing-library/react'
+import NextVid from './components/NextVid'
 
 
 
 
 
+ 
 class App extends React.Component {
+state ={
+  content:[]
+}
 
-
+componentDidMount() {
+  axios.get('/SideVideoSeed.json').then(
+    res => this.setState({content: res.data})
+  )
+}
   render() {
     return (
       <div className="App">
         <NavBar handleSubmit={this.handleSubmit} />
         <CurrentVid/>
+        <NextVid vids={this.state.content} />
       </div>
 
     );
---
+++ sprint/src/components/NextVid/NextVid.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './NextVid.scss'
+
+export default function NextVid({ vids }) {
+    return (
+        vids.map(vid => {
+            return <div key={vid.id} className="NextVid">
+
+                <h1 className="NextVid__title"> {vid.title}</h1>
+                <h2 className="NextVid__channel"> {vid.channel}</h2>
+                <img src={vid.image} key={vid.id} className="NextVid__thumbnail" />
+            </div>
+        })
+    )
+}
