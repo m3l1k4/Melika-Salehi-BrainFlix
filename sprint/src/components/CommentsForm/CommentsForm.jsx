d6427ea646586fe0e28b2a5a54a52690dfd7fda6
m3l1k4 <melikaihelas@gmail.com>
2020-06-21 21:46:17 -0700

    icon image fixed

--- sprint/src/components/CommentsForm/CommentsForm.jsx
+++ sprint/src/components/CommentsForm/CommentsForm.jsx
@@ -7,7 +7,7 @@ export default function CommentsForm({ handleSubmit }) {
             <h3 className="comments-form__header"> 3 Comments</h3>
 
             <form onSubmit={handleSubmit} className="comments-form" id="submit-comments">
-            <img src='/assets/Images/Mohan-muruge.jpg' className="comments-form__icon"></img>
+           <div className="comments-form__icon"> </div>
                 <div className="comments-form__field">
                     <p className="comments-form__field--title">JOIN THE CONVERSATION</p>
                     <textarea name="newComment" placeholder="Add a new comment" id="comment-content" className="comments-form__field--comment"
--- sprint/src/components/NavBar/NavBar.jsx
+++ sprint/src/components/NavBar/NavBar.jsx
@@ -11,7 +11,9 @@ export default function NavBar({ handleSubmit }) {
 
                 <button className="NavBar__Search--button"><img src='assets/Icons/SVG/Icon-upload.svg' className="NavBar__Search--upload"></img>UPLOAD</button>
 
-                <img src='/assets/Images/Mohan-muruge.jpg' className="NavBar__Search--icon"></img>
+<div className="NavBar__Search--icon">
+</div>
+            
             </div>
 
 
