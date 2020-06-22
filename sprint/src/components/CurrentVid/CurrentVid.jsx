d78df56ee2ed1b2d3e5a3fcf81e1475e5ccc97e1
m3l1k4 <melikaihelas@gmail.com>
2020-06-21 22:54:32 -0700

    desktop vid player icon graphics

--- sprint/src/components/CurrentVid/CurrentVid.jsx
+++ sprint/src/components/CurrentVid/CurrentVid.jsx
@@ -4,14 +4,27 @@ import './CurrentVid.scss'
 export default function CurrentVid({ handleSubmit }) {
     return (
         <div className="CurrentVid">
-        <video  className="CurrentVid__player" poster="../assets/Images/video-list-0.jpg">
-            <div className="CurrentVid__components">
-            <img className="CurrentVid__components--play" src='/assets/Icons/SVG/Icon-play.svg' ></img>
-            <img className="CurrentVid__components--fs" src='/assets/Icons/SVG/Icon-fullscreen.svg' ></img>
-            <img className="CurrentVid__components--vol" src='/assets/Icons/SVG/Icon-volume.svg' ></img>
-            </div> 
+            <video className="CurrentVid__player" poster="../assets/Images/video-list-0.jpg">
 
-        </video>
+            </video>
+
+            <div className="CurrentVid__components">
+                <div className="CurrentVid__components--play">
+             <img  src='../assets/Icons/SVG/Icon-play.svg' ></img>
+                </div>
+        <div className="CurrentVid__components--slider">
+                       <div className="CurrentVid__components--slider__line" ></div> 
+                       <p className="CurrentVid__components--slider__time">0:00/0:42</p>
+
+        </div>
+                <div className="CurrentVid__components--rcontrols">
+                    <img  src='../assets/Icons/SVG/Icon-fullscreen.svg' ></img>
+                    <img  src='../assets/Icons/SVG/Icon-volume.svg' ></img>
+                </div>
+
+
+            </div>
+
         </div>
     )
 }
