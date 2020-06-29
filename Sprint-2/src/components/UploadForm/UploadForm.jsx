2d1fbc74c8c867afaf4fa99acd24f0a7477ec752
m3l1k4 <melikaihelas@gmail.com>
2020-06-28 19:46:45 -0700

    desktop styling

--- Sprint-2/src/components/UploadForm/UploadForm.jsx
+++ Sprint-2/src/components/UploadForm/UploadForm.jsx
@@ -9,27 +9,29 @@ export default function UploadForm({ handleSubmit }) {
             <h1 className="upload-video__header"> Upload Video</h1>
 
             <form onSubmit={handleSubmit} className="upload-video__form" id="submit-upload">
+                <div className="upload-video__form--body" >
+                    <div className="upload-video__form--body__visual">
+                        <p className="upload-video__form--body__visual--title">VIDEO THUMBNAIL </p>
+                        <img src='assets/Images/Upload-video-preview.jpg' className="upload-video__form--body__visual--img"></img>
+                    </div>
+                    <div className="upload-video__form--body__field">
+                        <p className="upload-video__form--body__field--title">TITLE YOUR VIDEO</p>
+                        <textarea name="newuploadtitle" placeholder="Add a title to your video" id="upload-content" className="upload-video__form--body__field--upload-title"
+                            required></textarea>
+                        <p className="upload-video__form--body__field--title">ADD A VIDEO DESCRIPTION</p>
+                        <textarea name="newuploaddesc" placeholder="Add a new upload" id="upload-content" className="upload-video__form--body__field--upload-desc"
+                            required></textarea>
 
-                <div className="upload-video__form--visual">
-                    <p className="upload-video__form--visual__title">VIDEO THUMBNAIL </p>
-                    <img src='assets/Images/Upload-video-preview.jpg' className="upload-video__form--visual__img"></img>
+                    </div>
+
                 </div>
-                <div className="upload-video__form--field">
-                    <p className="upload-video__form--field__title">TITLE YOUR VIDEO</p>
-                    <textarea name="newuploadtitle" placeholder="Add a title to your video" id="upload-content" className="upload-video__form--field__upload-title"
-                        required></textarea>
-                    <p className="upload-video__form--field__title">ADD A VIDEO DESCRIPTION</p>
-                    <textarea name="newuploaddesc" placeholder="Add a new upload" id="upload-content" className="upload-video__form--field__upload-desc"
-                        required></textarea>
+
+                <div className="upload-video__form--options">
+                    <button type="submit" className="upload-video__form--options__button"
+                        id="upload-submit-button">UPLOAD</button>
+                    <p className="upload-video__form--options__cancel">CANCEL</p>
 
                 </div>
-
-              <div className="upload-video__form--options">
-                <button type="submit" className="upload-video__form--options__button"
-                    id="upload-submit-button">UPLOAD</button>
-                <p className="upload-video__form--options__cancel">CANCEL</p>
-          
-                </div>  
             </form>
 
 
