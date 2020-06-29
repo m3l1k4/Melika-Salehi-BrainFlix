f811c896bf275c11724a0fd9a087dbd636e500fd
m3l1k4 <melikaihelas@gmail.com>
2020-06-28 21:29:35 -0700

    comments section flex adjustments

--- Sprint-2/src/components/CommentsForm/CommentsForm.jsx
+++ Sprint-2/src/components/CommentsForm/CommentsForm.jsx
@@ -6,18 +6,18 @@ const imagebg = {
 
 export default function CommentsForm({ handleSubmit }) {
     return (
-        <div className="coments-form">
-            <h3 className="comments-form__header"> 3 Comments</h3>
+        <div className="comments-form__section">
+            <h3 className="comments-form__section--header"> 3 Comments</h3>
 
-            <form onSubmit={handleSubmit} className="comments-form" id="submit-comments">
-                <div className="comments-form__icon" style={imagebg}> </div>
-                <div className="comments-form__field">
-                    <p className="comments-form__field--title">JOIN THE CONVERSATION</p>
-                    <textarea name="newComment" placeholder="Add a new comment" id="comment-content" className="comments-form__field--comment"
+            <form onSubmit={handleSubmit} className="comments-form__section--form" id="submit-comments">
+                <div className="comments-form__section--form__icon" style={imagebg}> </div>
+                <div className="comments-form__section--form__field">
+                    <p className="comments-form__section--form__field--title">JOIN THE CONVERSATION</p>
+                    <textarea name="newComment" placeholder="Add a new comment" id="comment-content" className="comments-form__section--form__field--comment"
                         required></textarea>
 
                 </div>
-                <button type="submit" className="comments-form__field--button"
+                <button type="submit" className="comments-form__section--form__field--button"
                     id="comment-submit-button">COMMENT</button>
             </form>
             <div className="previous-comments" id="comments"></div>
