70f430d36b9991d5c70f46d3476c354d9400e80a
m3l1k4 <melikaihelas@gmail.com>
2020-07-04 21:42:03 -0700

    server GET implemented

--- Sprint-3/back-end/server.js
+++ Sprint-3/back-end/server.js
@@ -3,24 +3,31 @@ const app = express()
 const port = process.env.PORT || process.argv[2] || 8080
 const bodyParser = require('body-parser')
 
-
-
-const sideVideoInfo = require('./sidevid.json');
-const videos = require ('./mainvid.json');
+const videos = require('./sidevid.json');
+const mainVid = require('./mainvid.json');
 
 app.use(bodyParser.json());
 
 
 
-
-
 app.get('/videos', (req, res) => {
   res.json(videos)
+  console.log(videos)
 })
 
-app.get('/sideVideoInfo', (req, res) => {
-  res.json(sideVideoInfo)
+app.get('/videos/:id', (req, res) => {
+ 
+  const checkStatus = mainVid.some(video => video.id === req.params.id);
+  console.log(checkStatus)
+
+  if (checkStatus) {
+    let test = mainVid.filter(video => video.id === req.params.id);
+    res.json(test[0])
+  }
+
 })
+
+
 
 // app.post('/students', (req, res) => {
 //   const { name, program, grade } = req.body
